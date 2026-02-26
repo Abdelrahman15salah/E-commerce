96e50dce40253c010be57061d38d1e7438c7ca08
bedo <armamessi15@gmail.com>
2026-02-26 13:46:46 +0200

    refactor user controller to implement pagination and standardize error responses

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,11 +1,15 @@
 import User from "../models/users.model.js";
-
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find();
+    const paginationnumber = req.query.limit || 100;
+    const page = req.query.page || 1;
+    const users = await User.find()
+      .select("name email role isactive")
+      .limit(paginationnumber)
+      .skip((page - 1) * paginationnumber);
     res.status(200).json({ status: "success", data: { users } });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ status: "error", message: error.message });
   }
 };
 
@@ -34,7 +38,7 @@ const addUser = async (req, res) => {
     const user = await User.create({ name, email, password, role });
     res.status(201).json(user);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ status: "error", message: error.message });
   }
 };
 const getUser = async (req, res) => {
@@ -47,7 +51,7 @@ const getUser = async (req, res) => {
     }
     res.status(200).json({ status: "success", data: { user } });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ status: "error", message: error.message });
   }
 };
 const updateUser = async (req, res) => {
@@ -62,7 +66,7 @@ const updateUser = async (req, res) => {
     }
     res.status(200).json(user);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ status: "error", message: error.message });
   }
 };
 const deleteUser = async (req, res) => {
@@ -75,7 +79,7 @@ const deleteUser = async (req, res) => {
     }
     res.status(200).json({ status: "success", data: null });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ status: "error", message: error.message });
   }
 };
 
